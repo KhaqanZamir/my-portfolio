df776675f59eb0569e651992e198ea92357cf439
mahsammalik <malik_mahsam@hotmail.com>
2022-05-18 18:28:23 +0500

    sss

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -9,6 +9,9 @@ export default function Skills() {
   var [nextCount, setNextCount] = useState(0);
   var [javascriptCount, setJavascriptCount] = useState(0)
   var [reduxCount, setReduxCount] = useState(0);
+  var [htmlCssCount, setHtmlCssCount] = useState(0);
+  var [bootstrapCount, setBootstrapCount] = useState(0);
+  let [tailwindCount, setTailwindCount] = useState(0);
 
   const animateReact = () => {
       var id = setInterval(frame, 20);
@@ -50,6 +53,39 @@ export default function Skills() {
         clearInterval(id);
       } else {
         setReduxCount(reduxCount++);
+      }
+    }
+  }
+
+  const animateHTMLCss = () => {
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (htmlCssCount > 95) {
+        clearInterval(id);
+      } else {
+        setHtmlCssCount(htmlCssCount++);
+      }
+    }
+  }
+
+  const animateBootstrap = () => {
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (bootstrapCount > 95) {
+        clearInterval(id);
+      } else {
+        setBootstrapCount(bootstrapCount++);
+      }
+    }
+  }
+
+  const animateTailwind = () => {
+    var id = setInterval(frame, 20);
+    function frame() {
+      if (tailwindCount > 95) {
+        clearInterval(id);
+      } else {
+        setTailwindCount(tailwindCount++);
       }
     }
   }
@@ -143,6 +179,66 @@ export default function Skills() {
                   }}
                   status='active'
                   percent={nextCount}
+                />
+            </VisibilitySensor>
+          </div>
+          <div className='skill'>
+            <h3>HTML/CSS</h3>
+            <VisibilitySensor
+                    onChange={isVisible => {
+                      if (isVisible) {
+                        animateHTMLCss();
+                      } 
+                    }}
+                  >
+                <Progress
+                  type="circle"
+                  strokeColor={{
+                    '0%': '#19194e',
+                    '100%': '#788097',
+                  }}
+                  status='active'
+                  percent={htmlCssCount}
+                />
+            </VisibilitySensor>
+          </div>
+          <div className='skill'>
+            <h3>Bootstrap</h3>
+            <VisibilitySensor
+                    onChange={isVisible => {
+                      if (isVisible) {
+                        animateBootstrap();
+                      } 
+                    }}
+                  >
+                <Progress
+                  type="circle"
+                  strokeColor={{
+                    '0%': '#19194e',
+                    '100%': '#788097',
+                  }}
+                  status='active'
+                  percent={bootstrapCount}
+                />
+            </VisibilitySensor>
+          </div>
+          <div className='skill'>
+            <h3>Tailwind</h3>
+            <VisibilitySensor
+                    onChange={isVisible => {
+                      if (isVisible) {
+                        animateTailwind();
+                      } 
+                    }}
+                  >
+                <Progress
+                  type="circle"
+                  strokeColor={{
+                    '0%': '#19194e',
+                    '100%': '#788097',
+                  }}
+                  status='active'
+                  percent={tailwindCount}
                 />
             </VisibilitySensor>
           </div>
--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -21,7 +21,6 @@ export default function Content() {
                     <Counters/>
                     <Services/>
                     <Skills/>
-                    <About/>
             </div>
         </div>
     </div>
